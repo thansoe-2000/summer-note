2e0b981d18d94eee7284b2125eeac8c584dfb885
Kyaw Soe Hla <codewarrior.ksh@gmail.com>
2023-08-18 13:05:55 +0630

    'summernote'

--- summernote/urls.py
+++ summernote/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -24,3 +24,8 @@ urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('', include('myapp.urls')),
 ]
+
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
